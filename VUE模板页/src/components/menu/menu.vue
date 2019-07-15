5f8d2a99e8c8db9b42dc8d0502b87a5c54714fc9
flushofdawn <895616372@qq.com>
2019-07-15 18:59:21 +0800

    edit menu

--- VUE模板页/src/components/menu/menu.vue
+++ VUE模板页/src/components/menu/menu.vue
@@ -4,66 +4,46 @@
         <el-radio-button :label="false">展开</el-radio-button>
         <el-radio-button :label="true">收起</el-radio-button>
       </el-radio-group>
-      <el-menu class="el-menu-vertical-demo" v-for="( item1 ,index1 ) in menuList" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
-        <el-submenu index='index1'>
-          <template slot="title">
-            <i :class="[item1.icon]" ></i>
-            <span slot="title">{{ item1.name }}</span>
-          </template>
-          <!-- 二级菜单循环-->
-          <template  v-for="( item2 ,index2 ) in item1.childList">
-            <el-submenu index="">
-              <span slot="title">{{ item2.name }}</span>
-              <!-- 三级菜单循环-->
-              <template v-for="( item3 ,index3 ) in item2.childList">
-                <el-menu-item index="">{{ item3.name }}</el-menu-item>
-              </template>
-
-            </el-submenu>
-          </template>
-        </el-submenu>
-      </el-menu>
-     <!-- 一级菜单循环-->
-      <!--<el-menu class="el-menu-vertical-demo" v-for="( item1 ,index1 ) in menuList" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+      <!-- 一级菜单循环-->
+      <el-menu class="el-menu-vertical-demo" v-for="( item1 ,index1 ) in menuList" @open="handleOpen" @close="handleClose" >
         <el-submenu index='index1'  v-if="item1.permission">
           <template slot="title">
             <i :class="[item1.icon]" ></i>
             <span slot="title">{{ item1.name }}</span>
           </template>
-          &lt;!&ndash; 二级菜单循环&ndash;&gt;
+          <!-- 二级菜单循环 -->
           <template  v-for="( item2 ,index2 ) in item1.childList">
             <template v-if="item2.permission">
               <el-submenu index="index1+' - '+index2">
                 <span slot="title">{{ item2.name }}</span>
-                &lt;!&ndash; 三级菜单循环&ndash;&gt;
-
+               <!-- &lt;!&ndash; 三级菜单循环&ndash;&gt;-->
                 <template v-for="( item3 ,index3 ) in item2.childList">
                   <el-menu-item index="index1+' - '+index2+' - '+index3">{{ item3.name }}</el-menu-item>
                 </template>
-
               </el-submenu>
             </template>
-
             <template v-else="item2.permission">
               <el-menu-item index="index1+' - '+index2">{{ item2.name }}</el-menu-item>
             </template>
-
           </template>
-
         </el-submenu>
         <el-menu-item index='index1'  v-else="item1.permission">
           <i :class="[item1.icon]"></i>
           <span slot="title">{{ item1.name }}</span>
         </el-menu-item>
-      </el-menu>-->
+      </el-menu>
     </div>
 </template>
 
 <script type="text/ecmascript-6" >
   //import someComponent from './someComponent'
   import axios from 'axios';
+  import MenuList from '@/components/menu/menuList';
   export default {
       name: "Menu",
+      components: {
+        MenuList
+      },
       data(){
           return {
             menuList:'',
---
+++ VUE模板页/src/components/menu/menuList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div >
+
+
+     <!-- <el-submenu index='index1'  v-if="item1.permission">
+        <template slot="title">
+          <i :class="[item1.icon]" ></i>
+          <span slot="title">{{ item1.name }}</span>
+        </template>
+        &lt;!&ndash; 二级菜单循环&ndash;&gt;
+        <template  v-for="( item2 ,index2 ) in item1.childList">
+          <template v-if="item2.permission">
+            <el-submenu index="index1+' - '+index2">
+              <span slot="title">{{ item2.name }}</span>
+              &lt;!&ndash; 三级菜单循环&ndash;&gt;
+
+              <template v-for="( item3 ,index3 ) in item2.childList">
+                <el-menu-item index="index1+' - '+index2+' - '+index3">{{ item3.name }}</el-menu-item>
+              </template>
+
+            </el-submenu>
+          </template>
+
+          <template v-else="item2.permission">
+            <el-menu-item index="index1+' - '+index2">{{ item2.name }}</el-menu-item>
+          </template>
+
+        </template>
+
+      </el-submenu>
+      <el-menu-item index='index1'  v-else="item1.permission">
+        <i :class="[item1.icon]"></i>
+        <span slot="title">{{ item1.name }}</span>
+      </el-menu-item>-->
+    </div>
+</template>
+
+<script type="text/ecmascript-6" >
+  //import someComponent from './someComponent'
+  export default {
+      name: "MenuList",
+      data(){
+          return {
+          }
+      },
+      props:['dataList'],
+      methods: {
+
+      }
+  }
+</script>
+<style scoped>
+</style>
