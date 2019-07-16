9a5080d630d80c6ff094d7e16f7b82ee50ed6cfd
flushofdawn <895616372@qq.com>
2019-07-16 14:48:04 +0800

    edit menu

--- VUE模板页/src/components/menu/menu.vue
+++ VUE模板页/src/components/menu/menu.vue
@@ -5,30 +5,29 @@
         <el-radio-button :label="true">收起</el-radio-button>
       </el-radio-group>
       <!-- 一级菜单循环-->
-      <el-menu class="el-menu-vertical-demo" v-for="( item1 ,index1 ) in menuList" @open="handleOpen" @close="handleClose" >
-        <el-submenu index='index1'  v-if="item1.permission">
+      <el-menu class="el-menu-vertical-demo" v-for="( item1 ,index1 ) in menuList" :key="index1" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+        <el-submenu :index='toSting(index1)' v-if="item1.permission">
           <template slot="title">
             <i :class="[item1.icon]" ></i>
             <span slot="title">{{ item1.name }}</span>
           </template>
           <!-- 二级菜单循环 -->
-          <template  v-for="( item2 ,index2 ) in item1.childList">
-            <template v-if="item2.permission">
-              <el-submenu index="index1+' - '+index2">
+          <div  v-for="( item2 ,index2 ) in item1.childList" :key="index1+'-'+index2" >
+            <div v-if="item2.permission">
+              <el-submenu :index="toSting(index1,index2)">
                 <span slot="title">{{ item2.name }}</span>
-               <!-- &lt;!&ndash; 三级菜单循环&ndash;&gt;-->
-                <template v-for="( item3 ,index3 ) in item2.childList">
-                  <el-menu-item index="index1+' - '+index2+' - '+index3">{{ item3.name }}</el-menu-item>
-                </template>
+                <!-- 三级菜单循环 -->
+                <div v-for="( item3 ,index3 ) in item2.childList" :key="index1+'-'+index2+'-'+index3">
+                  <el-menu-item :index="toSting(index1,index2,index3)">{{ item3.name }}</el-menu-item>
+                </div>
               </el-submenu>
-            </template>
-            <template v-else="item2.permission">
-              <el-menu-item index="index1+' - '+index2">{{ item2.name }}</el-menu-item>
-
-            </template>
-          </template>
+            </div>
+            <div v-else="item2.permission">
+              <el-menu-item :index="toSting(index1,index2)">{{ item2.name }}</el-menu-item>
+            </div>
+          </div>
         </el-submenu>
-        <el-menu-item index='index1'  v-else="item1.permission">
+        <el-menu-item :index='toSting(index1)'  v-else="item1.permission">
           <i :class="[item1.icon]"></i>
           <span slot="title">{{ item1.name }}</span>
         </el-menu-item>
@@ -37,13 +36,12 @@
 </template>
 
 <script type="text/ecmascript-6" >
-  //import someComponent from './someComponent'
+
   import axios from 'axios';
-  import MenuList from '@/components/menu/menuList';
   export default {
       name: "Menu",
       components: {
-        MenuList
+
       },
       data(){
           return {
@@ -64,9 +62,18 @@
         getMenuList:function(){
           var This=this;
           axios.get("../static/data/menu.json", {}).then(function (response) {
-            console.log(  response.data.list )
             This.menuList = response.data.list;
           })
+        },
+        toSting:function(...arg){
+          let str='';
+          for( var i=0;i<arg.length;i++ ){
+            str += String( arg[i] ) + '-';
+          }
+          if( arg.length>0 ){
+            str = str.substr(0, str.length-1);
+          }
+          return str;
         }
       }
   }
