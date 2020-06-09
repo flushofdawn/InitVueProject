a6f3d6ee2cf855cef856817bd6050853d732ec34
flushofdawn <895616372@qq.com>
2020-06-09 17:29:00 +0800

    edit

--- VUE模板页/src/utils/tokenOpt.js
+++ VUE模板页/src/utils/tokenOpt.js
@@ -16,8 +16,38 @@
   }
 */
 export function getToken() {
-  return window.sessionStorage.getItem("token");
+  return cookie.get("token");
 }
 export function setToken(token) {
-  window.sessionStorage.setItem("token",token);
+  cookie.set("token",token,1);
+}
+
+
+export let cookie = {
+  //参数分别是cookie的名称，cookie的值，cookie的有效时间
+  set : function(key ,value, time) {
+    let date = new Date();
+    let expiresDays = time;
+    date.setTime(date.getTime() + expiresDays * 24 * 3600 *1000);
+    //这里是添加到已有的cookie中， 不要弄成+=
+    document.cookie = key + '=' + value + ";expores=" + date.toTimeString();
+  },
+  get : function(key) {
+    let getCookie = document.cookie.replace(/[ ]/g, "");
+    let arrCookie = getCookie.split(";");
+    let tips;
+    for (let i = 0; i != arrCookie.length; i++) {
+      let arr = arrCookie[i].split('=');
+      if (key == arr[0]) {
+        tips = arr[1];
+        break;
+      }
+    }
+    return tips;
+  },
+  delete : function(key) {
+    let date = new Date();
+    date.setTime(date.getTime()-10000000);
+    document.cookie = key + '= ;expores=' + date.toTimeString();
+  }
 }
